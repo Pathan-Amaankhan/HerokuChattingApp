7743b3a165132be093f889956d0c2759e2ce42ca
amaanpathan5@gmail.com <AkRocks>
2020-07-05 14:37:52 +0530

    heroku

--- server.ts
+++ server.ts
@@ -3,20 +3,36 @@ const mysql = require('mysql')
 
 const app = express();
 
-let connection = mysql.createConnection({
+const db_config = {
   host: 'us-cdbr-east-02.cleardb.com',
   user: 'bab8b42fad4e3f',
   password: '1ff11387',
   database: 'heroku_add251950d51535'
-});
+};
+let connection: any;
 
-connection.connect(function (error: any) {
-  if(!!error){
-    console.log('Error');
-  } else {
-    console.log('connected');
-  }
-});
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err: any) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err: any) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 app.use((req: any, res: any, next: any) => {
   res.header('Access-Control-Allow-Origin','*');
